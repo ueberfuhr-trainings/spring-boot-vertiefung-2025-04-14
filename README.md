fe32488de689aeee4c2189b9dbfff1a454dceebf
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2025-03-24 11:35:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Customer Management API Provider
+
+This is a Spring Boot sample for implementing a REST API.
+
+## Work with the code
+
+```bash
+# build the project
+mvn package
+# run the application
+java -jar target/customer-api-provider-0.0.1-SNAPSHOT.jar
+# or run the main class from IDE
+```
---
+++ src/main/java/com/samples/customers/CustomerManagementApiProviderApplication.java
@@ -0,0 +1,13 @@
+package com.samples.customers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerManagementApiProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerManagementApiProviderApplication.class, args);
+	}
+
+}
