9a0a341cbefa67af0bc2edd2e0e8750afc3b035d
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2025-03-25 13:24:20 +0100

    Add Bean Validation

--- src/main/java/com/samples/customers/Customer.java
+++ src/main/java/com/samples/customers/Customer.java
@@ -1,6 +1,10 @@
 package com.samples.customers;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,9 +17,14 @@ public class Customer {
 
   @JsonProperty(access = JsonProperty.Access.READ_ONLY)
   private UUID uuid;
+  @NotNull
+  @Size(min = 3, max = 100)
   private String name;
   // @JsonProperty("birth_date")
+  @NotNull
+  @Past
   private LocalDate birthdate;
+  @Pattern(regexp = "active|locked|disabled")
   private String state;
 
 }
--- src/test/java/com/samples/customers/ApiTests.java
+++ src/test/java/com/samples/customers/ApiTests.java
@@ -1,11 +1,16 @@
 package com.samples.customers;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.endsWith;
@@ -142,5 +147,82 @@ class ApiTests {
 
   }
 
+  @ParameterizedTest
+  @ValueSource(strings = {
+    // no name
+    """
+        {
+          "birthdate": "1985-07-03",
+          "state": "active"
+        }
+      """,
+    // short name
+    """
+        {
+          "name": "T",
+          "birthdate": "1985-07-03",
+          "state": "active"
+        }
+      """,
+    // long name
+    """
+        {
+          "name": "T0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789",
+          "birthdate": "1985-07-03",
+          "state": "active"
+        }
+      """,
+    // no birthdate
+    """
+        {
+          "name": "Tom Mayer",
+          "state": "active"
+        }
+      """,
+    // invalid birthdate
+    """
+        {
+          "name": "Tom Mayer",
+          "birthdate": "gelbekatze",
+          "state": "active"
+        }
+      """,
+    // invalid state
+    """
+        {
+          "name": "Tom Mayer",
+          "birthdate": "1985-07-03",
+          "state": "gelbekatze"
+        }
+      """,
+  })
+  void shouldNotCreateInvalidCustomer(String body) throws Exception {
+    mockMvc
+      .perform(
+        post("/customers")
+          .contentType(MediaType.APPLICATION_JSON)
+          .content(body)
+          .accept(MediaType.APPLICATION_JSON)
+      )
+      .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void shouldNotCreateCustomerWithBirthdateInTheFuture() throws Exception {
+    shouldNotCreateInvalidCustomer(
+      String.format("""
+          {
+            "name": "Tom Mayer",
+            "birthdate": "%s",
+            "state": "gelbekatze"
+          }
+          """,
+        LocalDate
+          .now()
+          .plusDays(1)
+          .format(DateTimeFormatter.ISO_DATE)
+      )
+    );
+  }
 
 }
