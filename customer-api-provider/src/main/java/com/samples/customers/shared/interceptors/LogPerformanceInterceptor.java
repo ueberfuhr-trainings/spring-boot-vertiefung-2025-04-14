197963fbe1e197029d397dfe40d1b0e62590c03e
Ralf Ueberfuhr <40685729+ueberfuhr@users.noreply.github.com>
2025-04-16 12:42:21 +0200

    Make logging level for LogPerformanceInterceptor configurable. (#7)
    Co-authored-by: Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>

--- customer-api-provider/src/main/java/com/samples/customers/shared/interceptors/LogPerformanceInterceptor.java
+++ customer-api-provider/src/main/java/com/samples/customers/shared/interceptors/LogPerformanceInterceptor.java
@@ -1,8 +1,10 @@
 package com.samples.customers.shared.interceptors;
 
+import com.samples.customers.shared.utilities.AnnotationUtility;
 import lombok.extern.slf4j.Slf4j;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.slf4j.event.Level;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,16 +13,26 @@ public class LogPerformanceInterceptor implements MethodInterceptor {
 
   @Override
   public Object invoke(MethodInvocation invocation) throws Throwable {
+    final var logPerformanceAnnotation = AnnotationUtility
+      .findAnnotation(
+        invocation.getMethod(),
+        LogPerformance.class
+      );
+    final var logLevel = logPerformanceAnnotation
+      .map(LogPerformance::value)
+      .orElse(Level.INFO);
     final var timeStampBefore = System.currentTimeMillis();
     try {
       return invocation.proceed();
     } finally {
       final var timeStampAfter = System.currentTimeMillis();
-      log.info(
-        "Method invoked: {} ({}ms)",
-        invocation.getMethod().getName(),
-        timeStampAfter - timeStampBefore
-      );
+      log
+        .atLevel(logLevel)
+        .log(
+          "Method invoked: {} ({}ms)",
+          invocation.getMethod().getName(),
+          timeStampAfter - timeStampBefore
+        );
     }
   }
 
